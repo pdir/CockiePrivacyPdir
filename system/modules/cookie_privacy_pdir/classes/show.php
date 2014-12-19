63c3a91b66fb494a8939631ae067b48dcf7bb5de
Mathias Arzberger <ma@pdir.de>
2014-12-19 15:47:53 +0100

    Release v0.1 stable

---
+++ system/modules/cookie_privacy_pdir/classes/show.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   cookiePrivacyPdir
+ * @author    pdir / digital agentur - Mathias Arzberger
+ * @license   LGPL
+ * @copyright 2014
+ */
+
+
+/**
+ * Namespace
+ */
+namespace cookiePrivacyPdir;
+
+
+/**
+ * Class show
+ *
+ * @copyright  2014
+ * @author     pdir / digital agentur - Mathias Arzberger
+ * @package    Devtools
+ */
+class show extends \Module
+{
+
+	/**
+	 * Template
+	 * @var string
+	 */
+	protected $strTemplate = 'ce_cookie_privacy';
+
+
+	/**
+	 * Generate the module
+	 */
+	protected function compile()
+	{		
+		$GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/cookie_privacy_pdir/assets/js/jquery.cookiebar.js';
+		
+		$pdirPrivacyCSS = $this->arrData['pdir_privacy_cookie_css'];
+		if($pdirPrivacyCSS != 'nocss') {
+			$GLOBALS['TL_CSS'][] = 'system/modules/cookie_privacy_pdir/assets/css/jquery.cookiebar.css';
+		}
+		if($pdirPrivacyCSS != 'nocss' AND $pdirPrivacyCSS != 'classic') {
+			$GLOBALS['TL_CSS'][] = 'system/modules/cookie_privacy_pdir/assets/css/jquery.cookiebar.css';
+			$GLOBALS['TL_CSS'][] = 'system/modules/cookie_privacy_pdir/assets/css/'.$pdirPrivacyCSS.'.css';
+		}
+		
+		if($this->arrData['pdir_privacy_cookie_accept_button'] == 1)
+			$this->Template->AcceptButton = 'true';
+		else 
+			$this->Template->AcceptButton = 'false';
+		
+		if($this->arrData['pdir_privacy_cookie_policy_button'] == 1)
+			$this->Template->PolicyButton = 'true';
+		else
+			$this->Template->PolicyButton = 'false';		
+
+		if($this->arrData['pdir_privacy_cookie_policy_popup_fixed'] == 1)
+			$this->Template->PopupFixed = 'true';
+		else
+			$this->Template->PopupFixed = 'false';
+		
+		if($this->arrData['pdir_privacy_cookie_policy_popup_bottom'] == 1)
+			$this->Template->PopupBottom = 'true';
+		else
+			$this->Template->PopupBottom = 'false';
+		
+		$pdirPopupType = $this->arrData['pdir_privacy_cookie_type'];
+		$this->Template->PopupType = $pdirPopupType;
+		
+		$pageID = $this->arrData['pdir_privacy_cookie_infopage'];
+		if (($jumpTo = \PageModel::findByPk($pageID)) !== null) {
+			$pdirInfoPage = \Controller::generateFrontendUrl($jumpTo->row());
+			$this->Template->InfoPage = $pdirInfoPage;
+		} else {
+			$this->Template->PolicyButton = 'false';
+		}
+		
+	}
+}
---
+++ system/modules/cookie_privacy_pdir/languages/de/modules.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   cookiePrivacyPdir
+ * @author    pdir / digital agentur - Mathias Arzberger
+ * @license   LGPL
+ * @copyright 2014
+ */
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD'][''] = array('', '');
+
+
+
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['cookiePrivacyPdir'] = 'Cookie Privacy / pdir.de';
+$GLOBALS['TL_LANG']['FMD']['cookiePrivacyView'] = array('Cookie Privacy Modul', 'CPM DESC');
+
+
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_popupType'] = array('Darstellungstyp', 'Es gibt 2 Möglichkeiten das Modul einzubinden, als Banner im Kopf- oder Fußbereich oder als ganzseitiges Overlay.');
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_cssType'] = array('Theme', 'Wähle einen CSS Theme oder verwende eigenes CSS und wähle "nicht laden".');
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_infoPage'] = array('Informationsseite', 'Link zur Informationseite wo der Besucher mehr über die Privatsphäre Einstellungen erfahren kann.');
+
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_acceptButton'] = array('Akzeptieren Button anzeigen?', '');
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_policyButton'] = array('Link zur Informationsseite anzeigen?', '');
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_popupFixed'] = array('Feste Position?', 'Setzt die Cookie Bar per CSS auf "position: fixed".');
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_popupBottom'] = array('Im Fußbereich?', 'Die Cookie Bar am Browserende angezeigt.');
+
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_popup_type_select']['type1'] = 'Banner';
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_popup_type_select']['type2'] = 'Popup (noch nicht implementiert)';
+
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_css_type_select']['nocss'] = 'Nicht laden';
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_css_type_select']['classic'] = 'Classic';
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_css_type_select']['white'] = 'Weiß';
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_css_type_select']['black'] = 'Schwarz';
+$GLOBALS['TL_LANG']['FMD']['COOKIE_PRIVACY_PDIR']['pdir_css_type_select']['blue'] = 'Blau';
